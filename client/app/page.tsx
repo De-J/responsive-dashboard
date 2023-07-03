57d2d9822014326207ebe56cb36cf9a3027398d7
De-J <devjtitan@gmail.com>
2023-07-03 15:36:26 +0530

    responsive fixes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,6 +6,7 @@ import Login from '@/components/Login';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1 className={styles.hide}>Board.</h1>
       <aside className={styles.aside}>
         <h1 className={styles.brand}>Board.</h1>
       </aside>
